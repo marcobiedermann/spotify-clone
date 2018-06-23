27d8d23070590f2c7e3d20bd406b9b18d76c2738
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-23 23:12:04 +0200

    add album page

--- client/assets/js/pages/Albums/index.jsx
+++ client/assets/js/pages/Albums/index.jsx
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Route } from 'react-router-dom';
+import AlbumPage from './Album';
 
-const AlbumsPage = () => (
+const AlbumsPage = ({ match }) => (
   <div>
     Albums
+    <Route path={`${match.url}/:album_id`} component={AlbumPage} />
   </div>
 );
+
+AlbumsPage.propTypes = {
+  match: PropTypes.shape(),
+};
+
+AlbumsPage.defaultProps = {
+  match: null,
+};
 
 export default AlbumsPage;
