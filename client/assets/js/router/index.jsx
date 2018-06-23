962f390041ccdffd7c95b1f21c9f58c2fe1063e7
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-23 18:50:26 +0200

    add browse page

--- client/assets/js/router/index.jsx
+++ client/assets/js/router/index.jsx
@@ -2,12 +2,14 @@ import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import history from './history';
+import BrowsePage from '../pages/Browse';
 import IndexPage from '../pages/Index';
 
 const Router = () => (
   <ConnectedRouter history={history}>
     <div>
       <Route path="/" exact component={IndexPage} />
+      <Route path="/browse" exact component={BrowsePage} />
     </div>
   </ConnectedRouter>
 );
