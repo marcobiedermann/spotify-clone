7ee418ee102706d22029ee79088d939f12716b2a
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-23 23:17:31 +0200

    add artists page

---
+++ client/assets/js/pages/Artists/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ArtistsPage = () => (
+  <div>
+    Artists
+  </div>
+);
+
+export default ArtistsPage;
--- client/assets/js/router/index.jsx
+++ client/assets/js/router/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import history from './history';
 import AlbumsPage from '../pages/Albums';
+import ArtistsPage from '../pages/Artists';
 import BrowsePage from '../pages/Browse';
 import IndexPage from '../pages/Index';
 
@@ -11,6 +12,7 @@ const Router = () => (
     <div>
       <Route path="/" exact component={IndexPage} />
       <Route path="/albums" component={AlbumsPage} />
+      <Route path="/artists" component={ArtistsPage} />
       <Route path="/browse" component={BrowsePage} />
     </div>
   </ConnectedRouter>
