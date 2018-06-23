19884bf5ff6e3e8d3ce7dfda537c9dfd38fce80a
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-23 19:51:02 +0200

    add categories page

---
+++ client/assets/js/pages/Browse/Categories/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CategoriesPage = () => (
+  <div>
+    Categories
+  </div>
+);
+
+export default CategoriesPage;
--- client/assets/js/pages/Browse/index.jsx
+++ client/assets/js/pages/Browse/index.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Route } from 'react-router-dom';
+import CategoriesPage from './Categories';
 import FeaturedPlaylistsPage from './FeaturedPlaylists';
 
 const BrowsePage = ({ match }) => (
   <div>
     Browse
+    <Route path={`${match.url}/categories`} exact component={CategoriesPage} />
     <Route path={`${match.url}/featured-playlists`} exact component={FeaturedPlaylistsPage} />
   </div>
 );
