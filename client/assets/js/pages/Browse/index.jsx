d417b8470bd38ff670f23a308c016e44188758fb
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-23 19:49:52 +0200

    add prop-type validation

--- client/assets/js/pages/Browse/index.jsx
+++ client/assets/js/pages/Browse/index.jsx
@@ -1,12 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import FeaturedPlaylistsPage from './FeaturedPlaylists';
 
-const BrowsePage = props => (
+const BrowsePage = ({ match }) => (
   <div>
     Browse
-    <Route path={`${props.match.url}/featured-playlists`} exact component={FeaturedPlaylistsPage} />
+    <Route path={`${match.url}/featured-playlists`} exact component={FeaturedPlaylistsPage} />
   </div>
 );
+
+BrowsePage.propTypes = {
+  match: PropTypes.shape(),
+};
+
+BrowsePage.defaultProps = {
+  match: null,
+};
 
 export default BrowsePage;
