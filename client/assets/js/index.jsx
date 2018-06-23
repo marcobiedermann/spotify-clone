d0c9ab2f6dddfe4de95c918f8d7be78f71b23273
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-23 12:49:49 +0200

    add react

---
+++ client/assets/js/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div>
+    Hello, world!
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
