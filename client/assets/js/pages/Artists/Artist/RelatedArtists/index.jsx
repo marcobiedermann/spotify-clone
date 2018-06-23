e616475674349a5a664758bc3afae1828ace6c1f
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-23 23:38:22 +0200

    add artist related artists page

---
+++ client/assets/js/pages/Artists/Artist/RelatedArtists/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RelatedArtistsPage = () => (
+  <div>
+    Related Artists
+  </div>
+);
+
+export default RelatedArtistsPage;
--- client/assets/js/pages/Artists/Artist/index.jsx
+++ client/assets/js/pages/Artists/Artist/index.jsx
@@ -2,12 +2,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import AlbumsPage from './Albums';
+import RelatedArtistsPage from './RelatedArtists';
 import TopTracksPage from './TopTracks';
 
 const ArtistPage = ({ match }) => (
   <div>
     Artist
     <Route path={`${match.url}/albums`} exact component={AlbumsPage} />
+    <Route path={`${match.url}/related-artists`} exact component={RelatedArtistsPage} />
     <Route path={`${match.url}/top-tracks`} exact component={TopTracksPage} />
   </div>
 );
