232a07522098b41332e3cc04c7662409beaf90df
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-24 11:49:58 +0200

    add user playlists page

---
+++ client/assets/js/pages/Users/User/Playlists/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlaylistsPage = () => (
+  <div>
+    Playlists
+  </div>
+);
+
+export default PlaylistsPage;
--- client/assets/js/pages/Users/User/index.jsx
+++ client/assets/js/pages/Users/User/index.jsx
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Route } from 'react-router-dom';
+import PlaylistsPage from './Playlists';
 
-const UserPage = () => (
+const UserPage = ({ match }) => (
   <div>
     User
+    <Route path={`${match.url}/playlists`} exact component={PlaylistsPage} />
   </div>
 );
+
+UserPage.propTypes = {
+  match: PropTypes.shape(),
+};
+
+UserPage.defaultProps = {
+  match: null,
+};
 
 export default UserPage;
