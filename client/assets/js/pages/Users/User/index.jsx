6f837b39d5ed1b98aa63de949b474cfe1f3eb26e
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-24 11:48:31 +0200

    add user page

---
+++ client/assets/js/pages/Users/User/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserPage = () => (
+  <div>
+    User
+  </div>
+);
+
+export default UserPage;
--- client/assets/js/pages/Users/index.jsx
+++ client/assets/js/pages/Users/index.jsx
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Route } from 'react-router-dom';
+import UserPage from './User';
 
-const UsersPage = () => (
+const UsersPage = ({ match }) => (
   <div>
     Users
+    <Route path={`${match.url}/:user_id`} exact component={UserPage} />
   </div>
 );
+
+UsersPage.propTypes = {
+  match: PropTypes.shape(),
+};
+
+UsersPage.defaultProps = {
+  match: null,
+};
 
 export default UsersPage;
