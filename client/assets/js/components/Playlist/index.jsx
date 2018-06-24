de2fd51d7f7c386e17362dbc73ceb705ef9a6a11
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-24 22:58:47 +0200

    link playlists

--- client/assets/js/components/Playlist/index.jsx
+++ client/assets/js/components/Playlist/index.jsx
@@ -1,26 +1,40 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
-const Playlist = ({ name, images }) => (
+const Playlist = ({
+  id,
+  images,
+  name,
+  owner,
+}) => (
   <figure className="playlist">
     {images[0] && (
-      <img src={images[0].url} alt={name} />
+      <Link to={`/users/${owner.id}/playlists/${id}`}>
+        <img src={images[0].url} alt={name} />
+      </Link>
     )}
     <figcaption>
-      {name}
+      <Link to={`/users/${owner.id}/playlists/${id}`}>
+        {name}
+      </Link>
     </figcaption>
   </figure>
 );
 
 Playlist.propTypes = {
+  id: PropTypes.string,
   images: PropTypes.arrayOf(PropTypes.shape()),
   name: PropTypes.string,
+  owner: PropTypes.shape(),
 };
 
 Playlist.defaultProps = {
+  id: '',
   images: [],
   name: '',
+  owner: null,
 };
 
 export default Playlist;
