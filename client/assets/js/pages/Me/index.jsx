832b8132c2c3d388b7b5eda9ad94e6eefeca8253
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-24 11:46:42 +0200

    add me playlists page

--- client/assets/js/pages/Me/index.jsx
+++ client/assets/js/pages/Me/index.jsx
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Route } from 'react-router-dom';
+import PlaylistsPage from './Playlists';
 
-const MePage = () => (
+const MePage = ({ match }) => (
   <div>
     Me
+    <Route path={`${match.url}/playlists`} exact component={PlaylistsPage} />
   </div>
 );
+
+MePage.propTypes = {
+  match: PropTypes.shape(),
+};
+
+MePage.defaultProps = {
+  match: null,
+};
 
 export default MePage;
