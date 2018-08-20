dcb96950002a6be3aec8ff1a038e489502e3e879
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-20 16:07:36 +0200

    add Me snapshot test

---
+++ client/assets/js/components/Me/index.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Me from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<MemoryRouter><Me /></MemoryRouter>)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
