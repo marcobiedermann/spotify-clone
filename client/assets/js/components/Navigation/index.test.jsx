89e99e858916266a31de860f20dfba293dd955a6
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-20 16:06:17 +0200

    add Navigation snapshot test

---
+++ client/assets/js/components/Navigation/index.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import Navigation from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<MemoryRouter><Navigation /></MemoryRouter>)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
