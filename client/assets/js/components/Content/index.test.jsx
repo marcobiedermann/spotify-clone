5c86a662a57cc7a302137d963f9ac12b811491b8
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-20 15:56:10 +0200

    add Content snapshot test

---
+++ client/assets/js/components/Content/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Content from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Content />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
