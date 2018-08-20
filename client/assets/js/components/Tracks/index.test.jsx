25a21eceacf6906a19e8bc3d396f0f7f74d52900
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-20 15:57:40 +0200

    add Tracks snapshot test

---
+++ client/assets/js/components/Tracks/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Tracks from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Tracks />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
