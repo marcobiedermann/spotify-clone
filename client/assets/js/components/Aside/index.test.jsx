e85a307a77b75534c4a58ee2fc2641f2da907204
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-21 08:33:22 +0200

    add Aside snapshot test

---
+++ client/assets/js/components/Aside/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Aside from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Aside />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
