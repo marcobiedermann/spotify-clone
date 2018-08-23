caad7d0398481daa66ff6824235758bd685ff36b
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-23 20:19:00 +0200

    add PlaylistsPage snapshot test

---
+++ client/assets/js/pages/Me/Playlists/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import PlaylistsPage from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<PlaylistsPage />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
