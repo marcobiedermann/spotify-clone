f636fd3543777eb7f847e6aea8d020ea2d765237
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-23 20:16:40 +0200

    add RelatedArtistsPage snapshot test

---
+++ client/assets/js/pages/Artists/Artist/RelatedArtists/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import RelatedArtistsPage from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<RelatedArtistsPage />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
