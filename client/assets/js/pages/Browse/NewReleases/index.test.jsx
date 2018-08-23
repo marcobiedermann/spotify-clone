5300988e6b9aa1ab8fe6227faa1499c805a0d2ec
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-23 18:02:33 +0200

    add NewReleasesPage snapshot test

---
+++ client/assets/js/pages/Browse/NewReleases/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import NewReleasesPage from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<NewReleasesPage />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
