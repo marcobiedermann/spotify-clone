9fe88df366b68cdc9d0598c6a8ab4c118d420341
Marco Biedermann <marco.biedermann@gmx.de>
2018-09-04 23:51:52 +0200

    update Albums snapshot test

--- client/assets/js/components/Albums/index.test.jsx
+++ client/assets/js/components/Albums/index.test.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Albums from '.';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Albums />)
-    .toJSON();
+describe('Albums component', () => {
+  const albums = [
+    {
+      id: '123456',
+    },
+  ];
 
-  expect(tree).toMatchSnapshot();
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<MemoryRouter><Albums albums={albums} /></MemoryRouter>)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
