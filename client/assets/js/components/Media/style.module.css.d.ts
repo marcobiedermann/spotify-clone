dbb676d2fe7d5fdd6d7221b072c7688000cc0e0a
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-10 08:55:48 +0100

    fix: CSS Module
    Add missing CSS Module type definitions

---
+++ client/assets/js/components/Media/style.module.css.d.ts
@@ -0,0 +1,8 @@
+declare const styles: {
+  readonly media: string;
+  readonly media__body: string;
+  readonly media__object: string;
+  readonly 'media__object--left': string;
+  readonly 'media__object--right': string;
+};
+export = styles;
