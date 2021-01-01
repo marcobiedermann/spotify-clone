52f7842bfa61716c9d03cca560392f4154d819df
Marco Biedermann <marco.biedermann@gmx.de>
2021-01-01 11:07:33 +0100

    chore: Dependencies
    Update dependencies

--- client/assets/js/components/Button/index.tsx
+++ client/assets/js/components/Button/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/button-has-type */
+
 import classNames from 'classnames';
 import React, { FC } from 'react';
 import styles from './style.module.css';
--- client/assets/js/components/Image/index.tsx
+++ client/assets/js/components/Image/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+
 import React, { FC } from 'react';
 
 export interface ImageProps {
