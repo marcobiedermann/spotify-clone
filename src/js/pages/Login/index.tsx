0261778d58e82689a2544002f4f5fc5bccaea025
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2021-11-12 14:21:06 +0100

    chore: Vite
    Migrate from Webpack to Vite

---
+++ src/js/pages/Login/index.tsx
@@ -0,0 +1,24 @@
+import { stringify } from 'qs';
+import React, { FC } from 'react';
+import { Helmet } from 'react-helmet';
+import { RouteChildrenProps } from 'react-router-dom';
+
+const LoginPage: FC<RouteChildrenProps> = () => {
+  const query = stringify({
+    client_id: import.meta.env.SPOTIFY_CLIENT_ID,
+    redirect_uri: import.meta.env.SPOTIFY_REDIRECT_URI,
+    response_type: 'code',
+    scope: ['user-read-private', 'user-read-email'].join(' '),
+  });
+
+  return (
+    <>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <a href={`https://accounts.spotify.com/authorize?${query}`}>Login</a>
+    </>
+  );
+};
+
+export default LoginPage;
