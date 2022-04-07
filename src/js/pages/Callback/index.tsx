8e3133ee5161e7294287f31a00d24f59781664da
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-04-07 20:31:13 +0200

    refactor: Query String
    Use URLSearchParams to create query string

--- src/js/pages/Callback/index.tsx
+++ src/js/pages/Callback/index.tsx
@@ -1,13 +1,12 @@
-import { parse } from 'qs';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Navigate, useLocation } from 'react-router-dom';
 
 function CallbackPage(): JSX.Element {
   const { search } = useLocation();
-  const { code } = parse(search, {
-    ignoreQueryPrefix: true,
-  });
+
+  const urlSearchParams = new URLSearchParams(search);
+  const code = urlSearchParams.get('code');
 
   // store CODE in localStorage
   console.log({ code });
--- src/js/pages/Login/index.tsx
+++ src/js/pages/Login/index.tsx
@@ -1,15 +1,16 @@
-import { stringify } from 'qs';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-function LoginPage(): JSX.Element {
-  const query = stringify({
-    client_id: import.meta.env.SPOTIFY_CLIENT_ID,
-    redirect_uri: import.meta.env.SPOTIFY_REDIRECT_URI,
-    response_type: 'code',
-    scope: ['user-read-private', 'user-read-email'].join(' '),
-  });
+const urlSearchParams = new URLSearchParams({
+  client_id: import.meta.env.SPOTIFY_CLIENT_ID,
+  redirect_uri: import.meta.env.SPOTIFY_REDIRECT_URI,
+  response_type: 'code',
+  scope: ['user-read-private', 'user-read-email'].join(' '),
+});
 
+const query = urlSearchParams.toString();
+
+function LoginPage(): JSX.Element {
   return (
     <>
       <Helmet>
