70f9c8e707a542579d3dc59195d734aeec751b95
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-04-13 10:04:41 +0200

    feat: Login
    Add login functionality

---
+++ src/js/constants/spotify.ts
@@ -0,0 +1,7 @@
+const apiBaseUrl = import.meta.env.VITE_SPOTIFY_API_BASE_URL;
+const accountsBaseUrl = import.meta.env.VITE_SPOTIFY_ACCOUNTS_BASE_URL;
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+const redirectUri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+
+export { apiBaseUrl, accountsBaseUrl, clientId, clientSecret, redirectUri };
