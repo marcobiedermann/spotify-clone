865b5fb4abd63551977f481abea82cd6b4d45f61
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-04-16 20:12:27 +0200

    chore: vite
    Upgrade vite to `v5`

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
