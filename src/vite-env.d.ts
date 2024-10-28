4d871c86531b931e8bd408b808c80ad4c82c5e61
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-10-28 13:37:26 +0100

    chore: Vite
    Update Vite and ESLint

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
-  readonly VITE_SPOTIFY_ACCOUNT_BASE_UR: string;
-  readonly VITE_SPOTIFY_API_BASE_UR: string;
+  readonly VITE_SPOTIFY_ACCOUNT_BASE_URL: string;
+  readonly VITE_SPOTIFY_API_BASE_URL: string;
   readonly VITE_SPOTIFY_CLIENT_ID: string;
   readonly VITE_SPOTIFY_CLIENT_SECRET: string;
   readonly VITE_SPOTIFY_REDIRECT_URI: string;
